9d2043f6360c13ec8052e40166ae2b00743452b9
shamsiddin808 <marsitschool456@gmail.com>
2024-07-24 15:41:19 +0500

    edit start code

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -11,18 +11,20 @@ from data.config import ADMINS
 from loader import dp, bot
 
 fake_data = defaultdict(dict)
+
+
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}\n Kampyuterga tuwiw uchun ruxsat botiga hush kelibsiz", reply_markup=main_btn)
+    await message.answer(f"Salom, {message.from_user.full_name}\nKampyuterga tuwiw uchun ruxsat botiga hush kelibsiz",
+                         reply_markup=main_btn)
     fake_data[message.from_user.full_name]['user_name'] = message.from_user.full_name
-
-
 
 
 @dp.message_handler(text="ruxsat sorash")
 async def ruxsat_sorash(message: types.Message):
-    await message.answer("Nechida tuwmoqchisiz")
+    await message.answer("Nechida tuwmoqchisiz\n\nMasalan: 14:00")
     await Rampage.time.set()
+
 
 @dp.message_handler(content_types=["text"], state=Rampage.time)
 async def handle_rampage_time(message: types.Message, state: FSMContext):
@@ -30,22 +32,26 @@ async def handle_rampage_time(message: types.Message, state: FSMContext):
     fake_data[message.from_user.id]['rampage_vaqt'] = message.text
 
     await bot.send_message(259083453, f"Sizdan shu vaqt uchun - {message.text}\n"
-                                  f"Bu foydalanuvchi ruxsat so'radingiz - {message.from_user.id}", reply_markup=admin_check_ruxsat)
+                                      f"Bu foydalanuvchi ruxsat so'radingiz - {message.from_user.id}",
+                           reply_markup=admin_check_ruxsat)
     await message.answer("Sorov Yuborildi javobni kuting")
     user_id = message.from_user.id
     await state.finish()
 
-
+x
 from aiogram import types
+
 
 @dp.callback_query_handler(text="ha_ruxsat")
 async def process_callback_ha_ruxsat(callback_query: types.CallbackQuery):
     await callback_query.message.delete()
-    await callback_query.bot.send_message(user_id, "Sizga ruxsat berildi✅")
+    await callback_query.bot.send_message(user_id, "Sizga ruxsat berildi ✅")
+
+
 from aiogram.types import CallbackQuery
+
 
 @dp.callback_query_handler(text="yoq_ruxsat")
 async def handle_yoo_ruxsat_callback(callback_query: CallbackQuery):
     await callback_query.message.delete()
-    await callback_query.bot.send_message(user_id,"sizga ruxsat yoq❌")
-
+    await callback_query.bot.send_message(user_id, "Sizga ruxsat yoq ❌")
